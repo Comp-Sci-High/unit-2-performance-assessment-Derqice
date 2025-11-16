616b6eb49010ac575bb2295d1d615476d9b85709
Derqice <139995816+Derqice@users.noreply.github.com>
2025-11-16 23:17:06 +0000

    Finished

--- api.js
+++ api.js
@@ -2,6 +2,24 @@ const env = require('dotenv').config({ quiet: true });
 const prompt = require('prompt-sync')();
 const apiKey = process.env.apiKey2
 
+let question= prompt("Hello please enter your book search:")
+let limit= prompt("How many results do you want:")
+let info= prompt(  "Available fields:\n\n" +
+  "key, redirects, title, subtitle, alternative_title, alternative_subtitle,\n" +
+  "ebook_access, edition_count, edition_key, format, by_statement,\n" +
+  "publish_date, contributor, publish_place, publisher, first_sentence,\n" +
+  "author_name, author_alternative_name, subject\n" +
+  "has_fulltext, title_suggest, publish_year, language, number_of_pages_median,\n" +
+  "publisher_facet, author_facet, first_publish_year, ratings_count,\n" +
+  "readinglog_count, want_to_read_count, currently_reading_count, already_read_count\n\n" +
+  "What information do you want to show (comma-separated): ")
+let requestURL="https://openlibrary.org/search.json?q="+question+"&limit="+limit+"&fields="+info
+async function findBooks(url){
+let response= await fetch(url)
+let data= await response.json()
+console.log(data.docs)
+}
+findBooks(requestURL)
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
--- chatgpt.js
+++ chatgpt.js
@@ -2,6 +2,30 @@ const env = require('dotenv').config({ quiet: true });
 const prompt = require('prompt-sync')();
 const apiKey = process.env.apiKey1
 
+let requestURL="https://api.openai.com/v1/responses"
+let question= prompt("Hello what do you want?:")
+async function gptAPI(gptdata){
+    let options={
+        method:"POST",
+        headers:{
+        Authorization:"Bearer "+apiKey,
+        "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            model:"gpt-5.1",
+            input:question
+        })
+    }
+    let response= await fetch(requestURL, options)
+    let data= await response.json()
+    // console.log(data)
+    console.log(data.output[0].content[0].text)
+}
+
+gptAPI({
+    model:"gpt-5.1",
+    input:question
+})
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
